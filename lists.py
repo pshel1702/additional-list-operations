c86ed09102a674b095a7e651c3fbdcf1dca0443e
vagrant <vagrant@vagrant.vm>
2021-03-24 04:58:42 +0000

    Added function to calculate table of multiplication

---
+++ lists.py
@@ -0,0 +1,61 @@
+"""Additional List Practice
+
+Complete the following functions. To get a better idea of how each function
+should behave, see the examples in test_lists.py.
+"""
+
+def multiplication_table(n):
+    """
+    Returns a 2-D array containing an nxn multiplication table
+    where n is the argument passed in
+    """
+
+    #create a new list
+    #every element in the list will be multiplied by the current index of the loop
+    mult_table = []
+    orig_table = [1*num for num in range(1,n+1)]
+    row = []
+
+    for i in range (1,n+1):
+        row = [i * index_val for index_val in orig_table]
+        mult_table.append(row)
+    
+    return mult_table
+
+
+    # TODO: replace this with your code
+
+def find_common_items_minimum_index_sum(list1, list2):
+    """
+    Returns the common item(s) between the two lists which have the lowest index sum
+    (the sum of the index of the item in each list.) If there is a tie, return all
+    items with the lowest index sum.
+    """
+
+    # TODO: replace this with your code
+
+def replace_elements(arr):
+    """
+    Replaces each element in arr with the greatest item among the elements to its right,
+    and replace the last element with -1 since there are no elements to its right.
+    For example [9,1,3,8] would turn into [9, 8, 8, -1].
+    The input array arr will be modified and the function will return None.
+    """
+
+    # TODO: replace this with your code
+
+def add_to_array_form(array_form_of_number, integer_to_add):
+    """
+    For a non-negative integer N, the array-form of N is an array of its digits in left to right order.
+    Takes the array-form of a non-negative integer (`array_form_of_number`) as well as an integer to add to it (`integer_to_add`) and returns the array-form of their sum.
+    """
+
+    # TODO: replace this with your code
+
+if __name__ == "__main__":
+    from pathlib import Path
+    import sys
+    import pytest
+
+    if len(sys.argv) > 1 and sys.argv[1] == "--test":
+        pytest.main([f"test_{Path(__file__).name}"])
---
+++ test_lists.py
@@ -0,0 +1,65 @@
+"""Tests for lists.py"""
+
+from lists import (
+    multiplication_table,
+    find_common_items_minimum_index_sum,
+    replace_elements,
+    add_to_array_form,
+)
+
+def test_multiplication_table_three():
+    """Test that the return value of get_words_by_first_letter is a list."""
+    assert multiplication_table(3) == [[1,2,3],[2,4,6],[3,6,9]]
+
+def test_multiplication_table_empty():
+    """Test that get_words_by_first_letter returns words that start with a certain letter."""
+    assert multiplication_table(0) == []
+
+def test_find_common_items_minimum_index_sum_1():
+    list1 = ["Shogun","Tapioca Express","Burger King","KFC"]
+    list2 = ["Piatti","The Grill at Torrey Pines","Hungry Hunter Steakhouse","Shogun"]
+    assert find_common_items_minimum_index_sum(list1, list2) == ["Shogun"]
+
+def test_find_common_items_minimum_index_sum_2():
+    list1 = ["Shogun","Tapioca Express","Burger King","KFC"]
+    list2 = ["KFC","Shogun","Burger King"]
+    assert find_common_items_minimum_index_sum(list1, list2) == ["Shogun"]
+
+def test_find_common_items_minimum_index_sum_3():
+    list1 = ["Shogun","Tapioca Express","Burger King","KFC"]
+    list2 = ["KFC","Burger King","Tapioca Express","Shogun"]
+     # use set to make sure order does not affect test outcome
+    assert set(find_common_items_minimum_index_sum(list1, list2)) == set(["KFC","Burger King","Tapioca Express","Shogun"])
+
+def test_find_common_items_minimum_index_sum_4():
+    list1 = ["Shogun","Tapioca Express","Burger King","KFC"]
+    list2 = ["KNN","KFC","Burger King","Tapioca Express","Shogun"]
+     # use set to make sure order does not affect test outcome
+    assert set(find_common_items_minimum_index_sum(list1, list2)) == set(["KFC","Burger King","Tapioca Express","Shogun"])
+
+def test_find_common_items_minimum_index_sum_5():
+    list1 = ["KFC"]
+    list2 = ["KFC"]
+    assert find_common_items_minimum_index_sum(list1, list2) == ["KFC"]
+
+def test_replace_elements_multiple():
+    arr = [17,18,5,4,6,1]
+    assert replace_elements(arr) == None
+    assert arr == [18,6,6,6,1,-1]
+
+def test_replace_elements_single():
+    arr = [400]
+    assert replace_elements(arr) == None
+    assert arr == [-1]
+
+def test_add_to_array_form_1():
+    assert add_to_array_form( [1,2,0,0], 34) == [1,2,3,4]
+
+def test_add_to_array_form_2():
+    assert add_to_array_form([2,7,4], 181) == [4,5,5]
+
+def test_add_to_array_form_3():
+    assert add_to_array_form([2,1,5], 806) == [1,0,2,1]
+
+def test_add_to_array_form_4():
+    assert add_to_array_form( [9,9,9,9,9,9,9,9,9,9], 1) == [1,0,0,0,0,0,0,0,0,0,0]
